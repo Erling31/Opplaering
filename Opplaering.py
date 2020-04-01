825641e62737485bcdf0badeef0dbbd36a23f013
Erling31 <59337478+Erling31@users.noreply.github.com>
2020-04-01 18:23:46 +0200

    Lek med Tensorflow

--- Opplaering.py
+++ Opplaering.py
@@ -1,1 +1,28 @@
 # Test av Github
+
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = data.load_data()
+
+class_names = ['T-shirt', 'Trousers', 'Pullover', 'Dress', 'Coat', 'Sandal', 
+                'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images = train_images/255.0
+test_images = test_images/255.0
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28,28)),
+    keras.layers.Dense(128, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+])
+
+model.compile(optimizer="adam", Loss="sparse_categorical_crossentropy",
+metrics=["accuracy"])
+
+
+
